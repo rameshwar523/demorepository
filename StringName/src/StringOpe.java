1cc8835454d7ced1e4bfbd3549ecae65a30b981d
rameshwar523 <115647016+rameshwar523@users.noreply.github.com>
2022-10-13 20:31:56 +0530

    changes are done

--- StringName/src/StringOpe.java
+++ StringName/src/StringOpe.java
@@ -4,6 +4,8 @@ public class StringOpe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("VCT"+(10+20+20+10));
+		System.out.println("VCT"+(10+20+20+10));
+		
 	}
 
 }
