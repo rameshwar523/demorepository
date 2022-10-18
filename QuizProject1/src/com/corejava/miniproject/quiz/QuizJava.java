bd1a79850872b8fb16d50e84912b62b0636f93f1
pmohi <rameshwar.shinde.se@gmail.com>
2022-10-18 19:55:18 +0530

    OK

--- QuizProject1/src/com/corejava/miniproject/quiz/QuizJava.java
+++ QuizProject1/src/com/corejava/miniproject/quiz/QuizJava.java
@@ -5,9 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-
 public class QuizJava extends Display{
-	
 public static void main(String[] args) {
 Display d=new Display();
 d.menuMain();
